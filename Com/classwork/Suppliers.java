b6702b6e501044fe480a6e65707dd7a9c33f0fd8
Sweeny Mbuvi <sweenymbuvi@gmail.com>
2023-03-01 22:07:49 +0300

    customer class

---
+++ Com/classwork/Suppliers.java
@@ -0,0 +1,26 @@
+package Com.classwork;
+
+public class Suppliers extends Person {
+    public String supplierID;
+    public double salary;
+
+    public Suppliers(String supplierID,double salary,String Id,String name,String phoneNumber,String emailAddress){
+        super(Id,name,phoneNumber,emailAddress);
+        this.supplierID=supplierID;
+        this.salary=salary;
+    }
+
+    public float getMoneyDueToPay(){
+        return 0;
+    }
+
+    @Override
+    public float getCashFlow() {
+        return 0;
+    }
+
+    @Override
+    public void showDetails() {
+
+    }
+}
