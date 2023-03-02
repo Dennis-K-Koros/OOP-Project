6e1353474b4722171ec0435c3bc09c545c11dd1c
Sweeny Mbuvi <sweenymbuvi@gmail.com>
2023-03-02 14:44:26 +0300

    login

--- Com/classwork/Login.java
+++ Com/classwork/Login.java
@@ -19,50 +19,50 @@ public class Login extends JFrame implements ActionListener {
         frame.setLayout(null);
 
         panel1=new JPanel();
-        panel1.setLayout(new FlowLayout());
+        panel1.setLayout(new GridLayout(5,1,5,5));
         panel1.setBounds(50,50,150,200);
         Id=new JLabel("ID");
-        Id.setSize(60,60);
+        Id.setSize(60,30);
         panel1.add(Id);
         name=new JLabel("NAME");
-        name.setSize(60,60);
+        name.setSize(60,30);
         panel1.add(name);
         phoneNumber=new JLabel("PHONE NUMBER");
-        phoneNumber.setSize(60,60);
+        phoneNumber.setSize(60,30);
         panel1.add(phoneNumber);
         emailAddress=new JLabel("EMAIL ADDRESS");
-        emailAddress.setSize(60,60);
+        emailAddress.setSize(60,30);
         panel1.add(emailAddress);
         password=new JLabel("PASSWORD");
-        password.setSize(60,60);
+        password.setSize(60,30);
         panel1.add(password);
         frame.add(panel1);
 
 
 
         panel2=new JPanel();
-        panel2.setLayout(new FlowLayout());
+        panel2.setLayout(new GridLayout(5,1,5,5));
         panel2.setBounds(200,50,150,200);
         txtId=new JTextField();
-        txtId.setSize(50,50);
+        txtId.setSize(150,30);
         panel2.add(txtId);
         txtName=new JTextField();
-        txtName.setSize(50,50);
+        txtName.setSize(150,30);
         panel2.add(txtName);
         txtPhoneNumber=new JTextField();
-        txtPhoneNumber.setSize(50,50);
+        txtPhoneNumber.setSize(150,30);
         panel2.add(txtPhoneNumber);
         txtEmailAddress=new JTextField();
-        txtEmailAddress.setSize(50,50);
+        txtEmailAddress.setSize(150,30);
         panel2.add(txtEmailAddress);
         txtPassword = new JTextField();
-        txtPassword.setSize(50,50);
+        txtPassword.setSize(150,30);
         panel2.add(txtPassword);
         frame.add(panel2);
 
         panel3=new JPanel();
         panel3.setLayout(new GridLayout(1,3,5,5));
-        panel3.setBounds(50,250,200,40);
+        panel3.setBounds(50,300,300,40);
         btnSignUp=new JButton("SIGN UP");
         btnSignUp.setSize(100,100);
         panel3.add(btnSignUp);
@@ -74,6 +74,9 @@ public class Login extends JFrame implements ActionListener {
         panel3.add(btnCancel);
         frame.add(panel3);
         frame.setVisible(true);
+    }
+    public static void main(String []args){
+        Login login1=new Login();
     }
 
 
